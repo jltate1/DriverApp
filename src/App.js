2cf250e06e624d4c3318ec7359565f1a91c84e4a
jltate1 <jltate@g.clemson.edu>
2020-02-03 13:24:05 -0500

    testing again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. CPSC 4910 Project
+          Edit <code>src/App.js</code> and save to reload. CPSC 4910 Project, Testing something
         </p>
         <a
           className="App-link"
